8fec12cb31643ad422686e6eaae9b9f214d48fd7
Bilal <travisissa005@gmail.com>
2025-04-23 10:12:54 +0100

    Resolved merge conflicts

--- app/(auth)/sign-in/LoginScreen.tsx
+++ app/(auth)/sign-in/LoginScreen.tsx
@@ -1,13 +1,86 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { useEffect} from 'react' 
+import { useRouter } from 'expo-router'
+import { TextInput } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 
 const LoginScreen = () => {
-  return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+  const navigation = useNavigation();
+  const router = useRouter();
+  useEffect(() => {
+    headerShown: false;
+  },[])
+       return (
+    <View
+      style={{
+        padding:25,
+        paddingTop: 80,
+        backgroundColor: '#fff',
+        height: '100%',
+              }}>
+      <Text 
+      style={{
+        fontSize: 30, fontFamily: 'outfit-bold',color: '#86808056'}}>
+        Let's Sign You In !</Text>
+      <Text 
+      style={{
+        fontSize: 30, fontFamily: 'outfit-bold',color: '#86808056'}}>
+      Welcome Back</Text>
+      <Text 
+      style={{
+        fontSize: 30, fontFamily: 'outfit',color: '#86808056',}}>
+      You've been missed</Text>
+
+      <View style={{
+        marginTop: 50,
+        }}>
+        <Text style={{
+          fontFamily: 'outfit',  
+        }}>Email</Text>
+        <TextInput style={styles.input} placeholder='Enter Email'/>
+      </View>
+
+      <View style={{
+        marginTop: 50,
+        }}>
+        <Text style={{
+          fontFamily: 'outfit',  
+        }}>PassWord</Text>
+        <TextInput 
+        secureTextEntry={true}
+        style={styles.input} placeholder='Enter Password'/>
+      </View>
+
+
+     <TouchableOpacity
+     onPress ={()=>router.replace('../sign-up')}
+     style={{
+      padding:20,
+      marginTop: 20,
+      backgroundColor:'#fff',
+      borderRadius: 15,
+      borderWidth: 1, 
+     }}>
+       <Text style={{
+        color: '#86808056',
+        textAlign: 'center',
+       }}> Create Account</Text>
+     </TouchableOpacity>
+
+      </View>
   )
 }
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginTop: 10,
+  },
+});
 
 export default LoginScreen;
-
--- app/(auth)/sign-up/SignUpScreen.tsx
+++ app/(auth)/sign-up/SignUpScreen.tsx
@@ -1,7 +1,17 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useNavigation }  from 'expo-router'
 
 const SignUpScreen = () => {
+  const navigation=useNavigation();
+  
+
+  useEffect(()=>{
+    navigation.setOptions({
+      headerShown:false
+    })
+  })
+
   return (
     <View>
       <Text>SignUpScreen</Text>
